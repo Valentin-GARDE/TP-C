6059be38c1e7215eda1a38e8ba5f842e301d50c1
Valentin-GARDE <valentin.garde1@gmail.com>
2025-10-27 14:18:13 +0100

    Modifcation exercice 4 sur la structure

--- TP_5/Ex_4.c
+++ TP_5/Ex_4.c
@@ -1,3 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Etudiant {
+    char nom[20];
+    char prenom[20];
+    int note;
+};
+
+int main()
+{
+    struct Etudiant etu;
+    
+    int n;
+
+    printf("indiuer le nombre d'etudiants: ");
+    scanf("%d", &n);
+    struct Etudiant *etudiants = (struct Etudiant *)malloc(n * sizeof(struct Etudiant));
+    for (int i = 0; i < n; i++) 
+    {
+        printf("Entrez le nom, prenom et note de l'etudiant %d: ", i + 1);
+        scanf("%s %s %d", etudiants[i].nom, etudiants[i].prenom, &etudiants[i].note);
+    }
+    printf("\nVoici la liste des étudiant avec une note >= 10:\n");
+    for (int i = 0; i < n; i++) 
+    {
+        if (etudiants[i].note > 10) 
+        {
+            etudiants[i].note = etudiants[i].note + 1;
+        }
+        else if (etudiants[i].note <= 10 && etudiants[i].note >= 15) 
+        {
+            etudiants[i].note = etudiants[i].note + 0.5;
+        }
+    }
+    for (int i = 0; i < n; i++) 
+    {
+        if (etudiants[i].note >= 10) 
+        {
+            printf("Nom: %s, Prenom: %s, Note: %d\n", etudiants[i].nom, etudiants[i].prenom, etudiants[i].note);
+        }
+    }
+}
